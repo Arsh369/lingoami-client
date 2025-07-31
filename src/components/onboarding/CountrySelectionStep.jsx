fbd740e9c10595a2e8a1ad09c98d18a8d58c2c75
Arsh369 <playgames1797@gmail.com>
2025-07-31 09:36:22 +0530

    commit

--- src/components/onboarding/CountrySelectionStep.jsx
+++ src/components/onboarding/CountrySelectionStep.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import {useNavigate} from 'react-router-dom';
-import countries from "";
+import countries from "../../assets/countries.json";
 import { updateStep } from '../../store/onboardingSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
